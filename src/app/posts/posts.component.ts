26ca9a64694f88380006a4d8075b9cd1a6fcf5ef
vladusaw93 <59958848+vladusaw93@users.noreply.github.com>
2020-04-07 18:22:41 +0300

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PostsComponent implements OnInit {
 
   @Input()
-  posts: PostsComponent;
+  posts: PostsModel;
 
   constructor() {
   }
